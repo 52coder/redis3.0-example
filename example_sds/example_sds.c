9e5405ba24bc41dce23c50a1b6a683e0dcacccae
52coder <persever2009@126.com>
2019-04-13 00:55:45 +0800

    优化sds例子,version1.0

--- example_sds/example_sds.c
+++ example_sds/example_sds.c
@@ -5,12 +5,10 @@ int main()
 {
     sds foo = sdsnew("Hello world redis.");
     /*打印sds->len sds->free*/
-    printf("%zu\n",sdslen(foo));  
-    printf("%zu\n",sdsavail(foo));
+    printf("%zu %zu\n",sdslen(foo),sdsavail(foo));  
 
     sds foo_dup = sdsdup(foo);
-    printf("%zu\n",sdslen(foo_dup));
-    printf("%zu\n",sdsavail(foo_dup)); 
+    printf("%zu %zu\n",sdslen(foo_dup),sdsavail(foo_dup)); 
 
     if(0 == sdscmp(foo,foo_dup))
     {
@@ -21,12 +19,14 @@ int main()
     /*sdslen(foo) + 8 + 1*/
     printf("%zu\n",sdsAllocSize(foo));
 
-    printf("%zu\n",sdslen(foo));  
-    printf("%zu\n",sdsavail(foo));
+    /*将sds扩充至指定长度*/
+    /*执行函数后sdslen增加10,由于redis内存分配策略：小于1m,newlen*=2因此free为28*/
+    foo = sdsgrowzero(foo,sdslen(foo) + 10);
+    printf("%zu %zu\n",sdslen(foo),sdsavail(foo));  
+    printf("%zu\n",sdsAllocSize(foo));
 
     sdsfree(foo);
     sdsfree(foo_dup);
-
 
     return 0;
 }
