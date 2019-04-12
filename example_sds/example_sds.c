ffd0c87e46f6e32d5bb9c655acde6eb90dca62c7
52coder <persever2009@126.com>
2019-04-13 00:21:52 +0800

    完善sds使用例子

--- example_sds/example_sds.c
+++ example_sds/example_sds.c
@@ -4,10 +4,29 @@
 int main()
 {
     sds foo = sdsnew("Hello world redis.");
+    /*打印sds->len sds->free*/
+    printf("%zu\n",sdslen(foo));  
+    printf("%zu\n",sdsavail(foo));
 
-    printf("%zu\n",sdslen(foo));    
+    sds foo_dup = sdsdup(foo);
+    printf("%zu\n",sdslen(foo_dup));
+    printf("%zu\n",sdsavail(foo_dup)); 
+
+    if(0 == sdscmp(foo,foo_dup))
+    {
+        printf("foo foo_dup exactly the same binary string.\n");
+    }
+
+    /*返回给定sds分配的内存字节数*/
+    /*sdslen(foo) + 8 + 1*/
+    printf("%zu\n",sdsAllocSize(foo));
+
+    printf("%zu\n",sdslen(foo));  
+    printf("%zu\n",sdsavail(foo));
 
     sdsfree(foo);
+    sdsfree(foo_dup);
+
 
     return 0;
 }
